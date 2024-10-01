664ebcde87232ccc5dcc345a64ab288f9e97df32
Gabe <gsantosx98@gmail.com>
2024-10-01 15:01:25 -0700

    solution for fib iterative

--- recursion.js
+++ recursion.js
@@ -1,3 +1,16 @@
+// Fibonacci JS iterative solution
 function fibs(integer) {
-
+  let array = [0, 1];
+  // n > 1, define edge cases
+  if (integer === 0) return [];
+  if (integer === 1) return array.slice(0, 1);
+  // declare i depending on how many numbers are given, in this case i = 2
+  for (let i = 2; i < integer; i++) {
+    // fn = fn-1 + fn-2
+    let first = array[i - 1];
+    let second = array[i - 2];
+    let next = first + second;
+    array.push(next)
+  }
+  return array;
 }
